e0369bd14704ee27af7857b659a2674daa69dea1
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2024-07-14 14:12:52 +1000

    Remove TODO

--- src/Harri.SchoolDemoAPI.Client/StudentApiClient.cs
+++ src/Harri.SchoolDemoAPI.Client/StudentApiClient.cs
@@ -7,7 +7,7 @@ namespace Harri.SchoolDemoApi.Client
     /// <summary>
     /// .NET 8 REST Client for Harri.SchoolDemoAPI using RestSharp.
     /// 
-    /// Does not throw exceptions on failed requests. TODO make sure this is asserted in tests
+    /// Does not throw exceptions on failed requests.
     /// </summary>
     public class StudentApiClient : IStudentApiClient
     {
