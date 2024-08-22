5117583d86fd7003c4cc4c2c7c3c67602d232594
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2024-08-22 21:01:49 +1000

    Update appsettings.json references in tests

--- src/Tests/Harri.SchoolDemoAPI.Tests.E2E/TestBase/StudentApiTestBase.cs
+++ src/Tests/Harri.SchoolDemoAPI.Tests.E2E/TestBase/StudentApiTestBase.cs
@@ -10,7 +10,7 @@ namespace Harri.SchoolDemoAPI.Tests.E2E.TestBase
         [OneTimeSetUp]
         public static void Setup()
         {
-            if (APIUrlToTest is null) throw new ArgumentException("APIUrlToTest from appsettings.json cannot be null");
+            if (APIUrlToTest is null) throw new ArgumentException("APIUrlToTest from testappsettings.json cannot be null");
 
             _client = new StudentApiClient(new HttpClient() { BaseAddress = new Uri(APIUrlToTest) });
         }
