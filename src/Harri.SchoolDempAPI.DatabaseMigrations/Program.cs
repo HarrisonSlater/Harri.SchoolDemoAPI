ca1a66d35ef2bb2008bed5b011ecf8c651805004
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2025-06-13 18:02:21 +1000

    Add appsettings.json, set TestScripts to run after Scripts

--- src/Harri.SchoolDempAPI.DatabaseMigrations/Program.cs
+++ src/Harri.SchoolDempAPI.DatabaseMigrations/Program.cs
@@ -1,34 +1,53 @@
 ﻿using DbUp;
+using DbUp.Engine;
+using DbUp.Support;
+using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
-static int Main(string[] args)
+namespace Harri.SchoolDempAPI.DatabaseMigrations
 {
-    var connectionString =
-        args.FirstOrDefault();
-        //?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
-
-    var upgrader =
-        DeployChanges.To
-            .SqlDatabase(connectionString)
-            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-            .LogToConsole()
-            .Build();
-
-    var result = upgrader.PerformUpgrade();
-
-    if (!result.Successful)
+    public class Program
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(result.Error);
-        Console.ResetColor();
-#if DEBUG
-        Console.ReadLine();
-#endif                
-        return -1;
-    }
+        static int Main(string[] args)
+        {
+            var projectName = "Harri.SchoolDempAPI.DatabaseMigrations";
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var sqlConnectionString = config["SQLConnectionString"];
+            // Use appsettings.json for default connection string
 
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Success!");
-    Console.ResetColor();
-    return 0;
+            var connectionString =
+                args.FirstOrDefault() ?? sqlConnectionString;
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException("SQL connection string is empty, use appsettings.json, or console argument");
+            }
+
+            var upgradeEngineBuilder = DeployChanges.To
+                .SqlDatabase(connectionString, null) //null or "" for default schema for user
+                // Script name for filtering looks like: "Harri.SchoolDempAPI.DatabaseMigrations.TestScripts.TestScript0001 - Create schools.sql"
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), script => script.StartsWith($"{projectName}.Scripts."), new SqlScriptOptions { ScriptType = ScriptType.RunOnce })
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), script => script.StartsWith($"{projectName}.TestScripts."), new SqlScriptOptions { ScriptType = ScriptType.RunOnce })
+                .LogToConsole(); //TODO integrate with API logging services
+
+            var upgrader = upgradeEngineBuilder.Build();
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+
+    }
 }
