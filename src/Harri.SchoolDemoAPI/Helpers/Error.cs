b311374a3bb49291a67522f9ec60ad97a4e56007
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2025-07-11 11:45:55 +1000

    Add result and error type from https://www.milanjovanovic.tech/blog/functional-error-handling-in-dotnet-with-the-result-pattern

---
+++ src/Harri.SchoolDemoAPI/Helpers/Error.cs
@@ -0,0 +1,8 @@
+﻿// See: https://www.milanjovanovic.tech/blog/functional-error-handling-in-dotnet-with-the-result-pattern
+namespace Harri.SchoolDemoAPI.Helpers
+{
+    public sealed record Error(string Code, string Description)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
---
+++ src/Harri.SchoolDemoAPI/Helpers/Result.cs
@@ -0,0 +1,30 @@
+﻿// See: https://www.milanjovanovic.tech/blog/functional-error-handling-in-dotnet-with-the-result-pattern
+using System;
+
+namespace Harri.SchoolDemoAPI.Helpers
+{
+    public class Result
+    {
+        private Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None ||
+                !isSuccess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(error));
+            }
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; }
+
+        public bool IsFailure => !IsSuccess;
+
+        public Error Error { get; }
+
+        public static Result Success() => new(true, Error.None);
+
+        public static Result Failure(Error error) => new(false, error);
+    }
+}
